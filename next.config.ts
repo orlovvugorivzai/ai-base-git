a822b6538f500079b4372c8f66e4db3f8c710caf
orlovvugorivzai <metamorf93@gmail.com>
2026-07-01 00:14:27 +0300

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/ai-base-git',
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
